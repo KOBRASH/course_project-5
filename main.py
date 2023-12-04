be17505d438505beba9ffdb66aa5e02f9588bc14
Sergei Aslanov <courageous@bk.ru>
2023-12-05 01:17:45 +0300

    .

--- main.py
+++ main.py
@@ -1,43 +1,49 @@
 from db_manager import DBManager
 
 def main():
-    # Инициализация объекта DBManager для работы с базой данных
-    db_manager = DBManager(
-        dbname='your_dbname',
-        user='your_user',
-        password='your_password',
-        host='your_host',
-        port='your_port'
-    )
+    db_manager = DBManager()
 
     while True:
-        print("Меню:")
-        print("1. Получить список всех компаний и количество вакансий")
-        print("2. Получить список всех вакансий")
-        print("3. Получить среднюю зарплату по вакансиям")
-        print("4. Получить список вакансий с зарплатой выше средней")
-        print("5. Получить список вакансий по ключевому слову")
+        print("\nМеню:")
+        print("1. Добавить компанию")
+        print("2. Добавить вакансию")
+        print("3. Получить список всех компаний и количество вакансий у каждой")
+        print("4. Получить список всех вакансий")
+        print("5. Получить среднюю зарплату по вакансиям")
+        print("6. Получить список вакансий с зарплатой выше средней")
+        print("7. Получить список вакансий по ключевому слову")
         print("0. Выйти")
 
         choice = input("Выберите действие: ")
 
         if choice == "1":
+            company_name = input("Введите название компании: ")
+            db_manager.insert_company_data(company_name)
+            print("Компания добавлена успешно.")
+        elif choice == "2":
+            company_id = int(input("Введите ID компании: "))
+            title = input("Введите название вакансии: ")
+            salary = float(input("Введите зарплату: "))
+            link = input("Введите ссылку на вакансию: ")
+            db_manager.insert_vacancy_data(company_id, title, salary, link)
+            print("Вакансия добавлена успешно.")
+        elif choice == "3":
             companies_vacancies_count = db_manager.get_companies_and_vacancies_count()
             print("Список всех компаний и количество вакансий:")
             print(companies_vacancies_count)
-        elif choice == "2":
+        elif choice == "4":
             all_vacancies = db_manager.get_all_vacancies()
             print("Список всех вакансий:")
             print(all_vacancies)
-        elif choice == "3":
+        elif choice == "5":
             avg_salary = db_manager.get_avg_salary()
             print("Средняя зарплата по вакансиям:")
             print(avg_salary)
-        elif choice == "4":
+        elif choice == "6":
             vacancies_higher_salary = db_manager.get_vacancies_with_higher_salary()
             print("Список вакансий с зарплатой выше средней:")
             print(vacancies_higher_salary)
-        elif choice == "5":
+        elif choice == "7":
             keyword = input("Введите ключевое слово для поиска вакансий: ")
             vacancies_with_keyword = db_manager.get_vacancies_with_keyword(keyword)
             print(f"Список вакансий с ключевым словом '{keyword}':")
@@ -47,7 +53,6 @@ def main():
         else:
             print("Неверный выбор. Пожалуйста, выберите существующую опцию.")
 
-    # Закрытие соединения с базой данных после работы
     db_manager.close_connection()
 
 if __name__ == "__main__":
